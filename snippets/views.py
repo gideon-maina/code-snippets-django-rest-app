8742fd7da4fd4232b46ab0b14777bfe6f209380b
Gideon Maina <maina.gidi@gmail.com>
2020-04-24 13:41:52 +0300

    task: snippets API using already mixed in generic views

--- snippets/views.py
+++ snippets/views.py
@@ -5,34 +5,17 @@ from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippletList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class SnippletList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
